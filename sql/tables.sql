35d729f43256e4af5f084de3f92754539f0ae8bd
Kara Spencer <kaspencer@csumb.edu>
2017-04-04 19:22:41 +0000

    Fixed my bad SQL table syntax

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,23 +1,26 @@
-create table eyemakeup{
-      id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
+create table eyemakeup (
+      id INT NOT NULL AUTO INCREMENT,
       brand varchar(55),
       itemName varchar(55),
-      price numeric(5,2)
+      price numeric(5,2),
+      PRIMARY KEY(id)
       
-}
+);
 
-create table facemakeup{
-      id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
+create table facemakeup (
+      id INT NOT NULL AUTO INCREMENT,
       brand varchar(55),
       itemName varchar(55),
-      price numeric(5,2)
+      price numeric(5,2),
+      PRIMARY KEY(id)
       
-}
+);
 
-create table skincare{
-      id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
+create table skincare (
+      id INT NOT NULL AUTO INCREMENT,
       brand varchar(55),
       itemName varchar(55),
-      price numeric(5,2)
+      price numeric(5,2),
+      PRIMARY KEY(id)
       
-}
+);
