ef72332f8ab581f9e612958d83ff8ac1f5629c6b
Kara Spencer <Kara Spencer>
2017-04-04 10:55:11 -0700

    Created SQL folder and tables

---
+++ sql/tables.sql
@@ -0,0 +1,23 @@
+create table eyemakeup{
+      id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
+      brand varchar(25),
+      itemName varchar(25),
+      price numeric(5,2)
+      
+}
+
+create table facemakeup{
+      id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
+      brand varchar(25),
+      itemName varchar(25),
+      price numeric(5,2)
+      
+}
+
+create table skincare{
+      id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
+      brand varchar(25),
+      itemName varchar(25),
+      price numeric(5,2)
+      
+}
