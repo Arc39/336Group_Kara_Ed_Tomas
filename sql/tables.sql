6d89d90967e813ec2a8f1b65b0d7829f69e17625
Kara Spencer <kaspencer@csumb.edu>
2017-04-04 18:37:58 +0000

    Made larger variable sizes

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,23 +1,23 @@
 create table eyemakeup{
       id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
-      brand varchar(25),
-      itemName varchar(25),
+      brand varchar(55),
+      itemName varchar(55),
       price numeric(5,2)
       
 }
 
 create table facemakeup{
       id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
-      brand varchar(25),
-      itemName varchar(25),
+      brand varchar(55),
+      itemName varchar(55),
       price numeric(5,2)
       
 }
 
 create table skincare{
       id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
-      brand varchar(25),
-      itemName varchar(25),
+      brand varchar(55),
+      itemName varchar(55),
       price numeric(5,2)
       
 }
