3eb13e0a87a9de1ff28b5970e0785a20cff84be6
Tomas Hernandez <tohernandez@csumb.edu>
2017-04-04 02:53:02 +0000

    Cleaned up some tabbing in the index page, and added CSS

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
-
 <!DOCTYPE html>
 <html>
     <head>
-        <title>makeup</title>
+        <title>Makeup Catalog</title>
+        <style>
+            @import url("assets/style.css");
+        </style>
     </head>
     <body>
         <div id = "wrapper">
+            <h3>Makeup Catalog</h3>
             <form>
-            
-            search for: <input type="text" name="brandName"/>
-            category: <select name="brandType">
-                          <option value="">Select One</option>
+                search for: 
+                <input type="text" name="brandName"/>
+                category: 
+                <select name="brandType">
+                    <option value="">Select One</option>
                           //<?php
                           //       $ = getDeviceTypes();
                           //       foreach ($deviceTypes as $deviceType) {
@@ -18,22 +22,17 @@
                           //       }
                           //     ?>
                     </select>
-                    <input type="checkbox" name="status" id="status"/>
-                    <label for="status">Check Availability</label>
-                    <br>
-                     
-                    Order By Price
-                    <br>
-                    <input type="radio" name="order" value="acs" id="cpcAcs"><label for="cpcAcs">low to high</label>
-                    <input type="radio" name="order" value="dec" id="cpcDec"><label for="cpcDec">high to low</label>
+                <input type="checkbox" name="status" id="status"/>
+                <label for="status">Check Availability</label>
+                <br>
+                Order By Price
+                <br>
+                <input type="radio" name="order" value="acs" id="cpcAcs"><label for="cpcAcs">low to high</label>
+                <input type="radio" name="order" value="dec" id="cpcDec"><label for="cpcDec">high to low</label>
                     
-                    <br>
-                    <input type="submit" value="Search" />
-            
-            
-            
+                <br>
+                <input type="submit" value="Search" />
             </form>
         </div>
-
     </body>
 </html>
